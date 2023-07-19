fbf2ea13ccc36833718b1d2f1120cea718eb82ce
kikiisa <kikiisa89@gmail.com>
2023-07-19 19:12:33 +0800

    beta 1.1

---
+++ resources/views/home/index.blade.php
@@ -0,0 +1,258 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="utf-8">
+    <meta content="width=device-width, initial-scale=1.0" name="viewport">
+
+    <title>Executive Computer</title>
+    <meta content="" name="description">
+    <meta content="" name="keywords">
+
+    <!-- Favicons -->
+    <link href="theme/img/excom.png" rel="icon">
+    <link href="theme/img/apple-touch-icon.png" rel="apple-touch-icon">
+
+    <!-- Google Fonts -->
+    <link
+        href="https://fonts.googleapis.com/css?family=Open+Sans:300,300i,400,400i,700,700i|Raleway:300,400,500,700,800"
+        rel="stylesheet">
+
+    <!-- Vendor CSS Files -->
+    <link href="theme/vendor/aos/aos.css" rel="stylesheet">
+    <link href="theme/vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
+    <link href="theme/vendor/bootstrap-icons/bootstrap-icons.css" rel="stylesheet">
+    <link href="theme/vendor/glightbox/css/glightbox.min.css" rel="stylesheet">
+    <link href="theme/vendor/swiper/swiper-bundle.min.css" rel="stylesheet">
+
+    <!-- Template Main CSS File -->
+    <link href="theme/css/style.css" rel="stylesheet">
+</head>
+
+<body>
+    <!-- ======= Header ======= -->
+    <header id="header" class="d-flex align-items-center bg-white">
+        <div class="container-fluid container-xxl d-flex align-items-center">
+
+            <div id="logo" class="me-auto">
+                <a href="index.html" class="scrollto"><img src="theme/img/excom.png" width="50" alt=""
+                        title=""></a>
+            </div>
+
+            <nav id="navbar" class="navbar order-last order-lg-0 text-dark ms-2">
+                <ul>
+                    <li><a class="text-dark nav-link scrollto active" href="#hero">Beranda</a></li>
+                    <li><a class="text-dark nav-link scrollto" href="#about">Tentang Kami</a></li>
+                    <li><a class="text-dark nav-link scrollto" href="#speakers">Layanan</a></li>
+                </ul>
+                <i class="bi bi-list mobile-nav-toggle text-dark"></i>
+            </nav><!-- .navbar -->
+            <a class="buy-tickets scrollto" href="{{ Route("login") }}">Login</a>
+            <a class="buy-tickets scrollto" href="{{ Route("register") }}">Registrasi</a>
+        </div>
+    </header><!-- End Header -->
+
+    <!-- ======= Hero Section ======= -->
+    <section id="hero">
+        <div class="hero-container" data-aos="zoom-in" data-aos-delay="100">
+            <h1 class="mb-4 pb-0"><br><span>Executive</span>Computer</h1>
+            <p class="mb-4 pb-0">Executive Computer / Indonesia - Gorontalo</p>
+            <a href="https://www.youtube.com/watch?v=jDDaplaOz7Q" class="glightbox play-btn mb-4"></a>
+        </div>
+    </section><!-- End Hero Section -->
+
+    <main id="main">
+        <section id="about">
+            <div class="container position-relative" data-aos="fade-up">
+                <div class="row">
+                    <div class="col-lg-12">
+                        <h2>Tentang Executive Computer</h2>
+                        <p>Executive computer adalah perusahaan jasa yang berfokus pada layanan service computer dengan komitmen untuk memberikan solusi dan perbaikan terbaik untuk perangkat elektronik pelanggan. Sebagai penyedia layanan service computer yang terpercaya, Excom memiliki tim teknisi berpengalaman dan terampil yang siap memberikan bantuan profesional dalam mengatasi masalah perangkat keras dan perangkat lunak komputer..</p>
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <section id="speakers">
+            <div class="container" data-aos="fade-up">
+                <div class="section-header">
+                    <h2>Layanan Kami</h2>
+                    <p>Pelayanan Executive Computer</p>
+                </div>
+
+                <div class="row justify-content-center">
+                    <div class="col-lg-4 col-md-6">
+                        <div class="speaker" data-aos="fade-up" data-aos-delay="100">
+                            <img src="theme/img/service_1.jpg" alt="Speaker 1" class="img-fluid">
+                            <div class="details">
+                                <h3><a href="speaker-details.html">Service  Computer</a></h3>
+                                
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="speaker" data-aos="fade-up" data-aos-delay="200">
+                            <img src="theme/img/service_2.jpg" alt="Speaker 2" class="img-fluid">
+                            <div class="details">
+                                <h3><a href="speaker-details.html">Instalasi Computer</a></h3>
+                               
+                                
+                            </div>
+                        </div>
+                    </div>
+                    <div class="col-lg-4 col-md-6">
+                        <div class="speaker" data-aos="fade-up" data-aos-delay="100">
+                            <img src="theme/img/service_3.jpeg" alt="Speaker 4" class="img-fluid">
+                            <div class="details">
+                                <h3><a href="speaker-details.html">Pembuatan Konveksi</a></h3>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <section id="faq">
+            <div class="container" data-aos="fade-up">
+                <div class="section-header">
+                    <h2>F.A.Q </h2>
+                </div>
+                <div class="row justify-content-center" data-aos="fade-up" data-aos-delay="100">
+                    <div class="col-lg-9">
+
+                        <ul class="faq-list">
+
+                            <li>
+                                <div data-bs-toggle="collapse" class="collapsed question" href="#faq1">Bagaimana
+                                    saya bisa menghubungi Excom untuk mendapatkan layanan service computer?<i
+                                        class="bi bi-chevron-down icon-show"></i><i
+                                        class="bi bi-chevron-up icon-close"></i></div>
+                                <div id="faq1" class="collapse" data-bs-parent=".faq-list">
+                                    <p>
+                                        Anda dapat menghubungi Excom melalui nomor telepon yang tertera di situs web
+                                        mereka atau mengirimkan email ke alamat yang disediakan. Selain itu, Excom juga
+                                        memiliki kantor fisik yang dapat Anda kunjungi untuk berkonsultasi langsung.
+                                    </p>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div data-bs-toggle="collapse" href="#faq2" class="collapsed question"> Berapa
+                                    estimasi biaya perbaikan untuk perangkat komputer yang mengalami masalah?<i
+                                        class="bi bi-chevron-down icon-show"></i><i
+                                        class="bi bi-chevron-up icon-close"></i></div>
+                                <div id="faq2" class="collapse" data-bs-parent=".faq-list">
+                                    <p>
+                                        Biaya perbaikan perangkat komputer dapat bervariasi tergantung pada jenis dan
+                                        tingkat kerusakan. Setelah melakukan diagnosa, tim teknisi Excom akan memberikan
+                                        estimasi biaya kepada Anda sebelum melakukan perbaikan.
+                                    </p>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div data-bs-toggle="collapse" href="#faq3" class="collapsed question">Apakah Excom
+                                    menyediakan layanan perbaikan di tempat atau harus membawa perangkat ke bengkel
+                                    mereka?<i class="bi bi-chevron-down icon-show"></i><i
+                                        class="bi bi-chevron-up icon-close"></i></div>
+                                <div id="faq3" class="collapse" data-bs-parent=".faq-list">
+                                    <p>
+                                        Biasanya, Excom menyediakan layanan perbaikan di tempat (on-site service) untuk
+                                        beberapa jenis masalah. Namun, jika diperlukan perbaikan lebih mendalam, mungkin
+                                        mereka akan meminta Anda untuk membawa perangkat ke bengkel mereka.
+                                    </p>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div data-bs-toggle="collapse" href="#faq4" class="collapsed question">Berapa lama
+                                    waktu yang dibutuhkan untuk melakukan perbaikan komputer di Excom?<i
+                                        class="bi bi-chevron-down icon-show"></i><i
+                                        class="bi bi-chevron-up icon-close"></i></div>
+                                <div id="faq4" class="collapse" data-bs-parent=".faq-list">
+                                    <p>
+                                        Waktu perbaikan bisa bervariasi tergantung pada tingkat kerusakan dan
+                                        ketersediaan komponen yang diperlukan. Biasanya, Excom berusaha untuk
+                                        menyelesaikan perbaikan dengan cepat dan efisien untuk meminimalkan
+                                        ketidaknyamanan pelanggan.
+                                    </p>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div data-bs-toggle="collapse" href="#faq5" class="collapsed question">Apakah Excom
+                                    memberikan garansi untuk layanan perbaikan yang diberikan?
+                                    <i class="bi bi-chevron-down icon-show"></i><i
+                                        class="bi bi-chevron-up icon-close"></i>
+                                </div>
+                                <div id="faq5" class="collapse" data-bs-parent=".faq-list">
+                                    <p>
+                                        Ya, Excom memberikan garansi untuk layanan perbaikan yang telah dilakukan.
+                                        Garansi tersebut mencakup masa jaminan tertentu untuk perbaikan yang telah
+                                        dilakukan, sehingga Anda dapat tenang jika terjadi masalah setelah perbaikan.
+                                    </p>
+                                </div>
+                            </li>
+
+                            <li>
+                                <div data-bs-toggle="collapse" href="#faq6" class="collapsed question">Apakah Excom
+                                    menerima layanan pengerjaan perangkat komputer dari luar kota? <i
+                                        class="bi bi-chevron-down icon-show"></i><i
+                                        class="bi bi-chevron-up icon-close"></i></div>
+                                <div id="faq6" class="collapse" data-bs-parent=".faq-list">
+                                    <p>
+                                        Sebagian besar Excom mungkin menerima layanan perbaikan dari luar kota, tetapi
+                                        hal ini dapat bervariasi tergantung pada lokasi dan kebijakan Excom. Sebaiknya
+                                        Anda menghubungi mereka terlebih dahulu untuk memastikan apakah mereka dapat
+                                        menerima layanan dari luar kota.
+                                    </p>
+                                </div>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </div>
+        </section>
+    </main>
+    <footer id="footer">
+        <div class="footer-top">
+            <div class="container">
+                <div class="row justify-content-center">
+
+                    <div class="col-lg-12 col-md-12 footer-info">
+                        <img src="theme/img/excom.png" width="50" alt="TheEvenet">
+                        <p>
+                            Executive Computer adalah perusahaan jasa yang berfokus pada layanan service computer dengan komitmen untuk memberikan solusi dan perbaikan terbaik untuk perangkat elektronik pelanggan. Sebagai penyedia layanan service computer yang terpercaya, Excom memiliki tim teknisi berpengalaman dan terampil yang siap memberikan bantuan profesional dalam mengatasi masalah perangkat keras dan perangkat lunak komputer.
+                        </p>
+                    </div>
+                        <div class="social-links">
+                            <a href="#" class="twitter"><i class="bi bi-twitter"></i></a>
+                            <a href="#" class="facebook"><i class="bi bi-facebook"></i></a>
+                            <a href="#" class="instagram"><i class="bi bi-instagram"></i></a>
+                            <a href="#" class="linkedin"><i class="bi bi-linkedin"></i></a>
+                        </div>
+
+                    </div>
+
+                </div>
+            </div>
+        </div>
+        <div class="container">
+            <div class="copyright">
+                &copy; Copyright <strong>Executive Computer</strong>. All Rights Reserved
+            </div>
+            <div class="credits">
+                Designed by <a href="https://bootstrapmade.com/">Executive Computer</a>
+            </div>
+        </div>
+    </footer>
+    <a href="#" class="back-to-top d-flex align-items-center justify-content-center"><i
+            class="bi bi-arrow-up-short"></i></a>
+    <script src="theme/vendor/aos/aos.js"></script>
+    <script src="theme/vendor/bootstrap/js/bootstrap.bundle.min.js"></script>
+    <script src="theme/vendor/glightbox/js/glightbox.min.js"></script>
+    <script src="theme/vendor/swiper/swiper-bundle.min.js"></script>
+    <script src="theme/vendor/php-email-form/validate.js"></script>
+    <script src="theme/js/main.js"></script>
+</body>
+</html>
